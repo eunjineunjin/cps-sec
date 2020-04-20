d44564bc6989bda73f4337ab1d3036893e72c612
eunjineunjin <45138692+eunjineunjin@users.noreply.github.com>
2020-04-20 21:16:53 +0900

    Add files via upload

--- indeed.py
+++ indeed.py
@@ -2,70 +2,59 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-class indeedScraper :
+class Scraper():
+    def __init__(self):
+        self.url = "http://kr.indeed.com/jobs?q=python&limit=50"
+        self.cnt = 0
 
-    def __init__(self) :
-        self.url = ""
-
-    def allPages(self, pages) :
-        res = requests.get(self.url + "&start=" + str(pages))
-
-        if res.status_code != 200 :
-            print("bad request", res.status_code)
-
+    def getHTML(self):
+        res = requests.get(self.url + "&start=" + str(self.cnt*50))
+        if res.status_code != 200:
+            print("request error : ", res.status_code)
         html = res.text
         soup = BeautifulSoup(html, "html.parser")
-
         return soup
 
-    def totalPage(self) :
-        res = requests.get(self.url)
-
-        if res.status_code != 200 :
-            print("bad request", res.status_code)
-
-        html = res.text
-        soup = BeautifulSoup(html, "html.parser")
-
+    def getPages(self, soup):
         pages = soup.select(".pagination > a")
-
         return len(pages)
 
-    def getContent(self, page) :
-        soup = self.allPages(page)
-        contents = soup.select("div.jobsearch-SerpJobCard")
+    def getCards(self, soup):
+        jobCards = soup.find_all("div", class_ = "jobsearch-SerpJobCard")
+        jobID = []
+        jobTitle = []
+        jobLocation = []
+        for j in jobCards:
+            jobTitle.append(j.find("a").text.replace("\n", ""))
+            if j.find("div", class_ = "location") != None:
+                jobLocation.append(j.find("div", class_ = "location").text)
+            elif j.find("span", class_ = "location") != None:
+                jobLocation.append(j.find("span", class_ = "location").text)
+            jobID.append("http://kr.indeed.com/viewjob?jk=" + j["data-jk"])
+        self.writeCSV(jobID, jobTitle, jobLocation)
 
-        cnt = 0
-        jobId = []
-        title = []
-
-        for c in contents :
-            jobId.append("" + c.attrs["data-jk"])
-            title.append(c.find("div", class_ = "title").find("a").text.replace("\n", ""))
-            cnt += 1
-
-        self.writeCSV(jobId, title)
-
-    def writeCSV(self, jobId, title) :
+    def writeCSV(self, ID, Title, Location):
         file = open('indeed.csv','a', newline='')
         wr = csv.writer(file)
-
-        for i in range(len(jobId)) :
-            wr.writerow([str(i), jobId[i], title[i]])
-
+        for i in range(len(ID)):
+            wr.writerow([str(i+1+self.cnt*50), ID[i], Title[i], Location[i]])
         file.close()
+        self.cnt = self.cnt + 1
 
-    def scrap(self) :
-        file = open('indeed.csv','w', newline='')
+    def scrap(self):
+        soupPage = self.getHTML(0)
+        pages = self.getPages(soupPage)
+        file = open('indeed.csv', 'w', newline='')
         wr = csv.writer(file)
-        wr.writerow(["No", "Link","title"])
+        wr.writerow(["No.", "Link","Title", "Location"])
         file.close()
 
-        page = self.totalPage()
+        for i in range(pages) :
+            soupCard = self.getHTML()
+            self.getCards(soupCard)
 
-        for i in range(page) :
-            self.getContent(i * 50)
+
 
 if __name__ == "__main__":
-    scraper = indeedScraper()
-    scraper.scrap()
+    s = Scraper()
+    s.scrap()
