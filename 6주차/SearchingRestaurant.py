ca512c871ba2455b8c21acd4216cd587e9e37c9b
eunjineunjin <45138692+eunjineunjin@users.noreply.github.com>
2020-04-29 05:10:21 +0900

    Add files via upload

--- 6주차/SearchingRestaurant.py
+++ 6주차/SearchingRestaurant.py
@@ -4,22 +4,57 @@ from bs4 import BeautifulSoup
 import os
 import time
 
-path = os.getcwd() + "/chromedriver.exe"
-driver = webdriver.Chrome(path)
+def Crawler(searchIndex):
+    path = os.getcwd() + "/chromedriver.exe"
+    driver = webdriver.Chrome(path)
 
-try:
-    #네이버 지도
-    driver.get("https://map.naver.com/v5")
-    time.sleep(5)
+    try:
+        #멜론에서 가수 검색
+        driver.get("https://www.melon.com/")
+        time.sleep(3)
 
-    searchIndex = "맛집"
-    element = driver.find_element_by_class_name("label_search")
-    element.send_keys(searchIndex)
-    #driver.find_element_by_class_name("btn_search").click()
+        element = driver.find_element_by_xpath('//*[@id="top_search"]')
+        element.send_keys(searchIndex)
+        driver.find_element_by_xpath('//*[@id="gnb"]/fieldset/button[2]/span').click()
+
+        driver.find_element_by_xpath('//*[@id="conts"]/div[4]/div/div/a').click()
+
+        #전체 페이지 수 찾기
+        html = driver.page_source
+        bs = BeautifulSoup(html, "html.parser")
+        pages = bs.find("span", class_ = "page_num").find_all("a")[-1].text
+
+        #각 페이지에서 노래 제목 추출
+        title = []
+
+        for i in range(int(pages)):
+            time.sleep(3)
+
+            html = driver.page_source
+            bs = BeautifulSoup(html, "html.parser")
+
+            conts = bs.find("div", id = "pageList").find_all("a", class_ = "fc_gray")
+
+            title.append("page: " + str(i + 1))
+            for c in conts:
+                title.append(c.text)
+
+            if i != int(pages) - 1:
+                driver.find_element_by_xpath('//*[@id="pageObjNavgation"]/div/span/a[' +str(i + 1) + ']').click()
+
+    finally:
+        for t in title:
+            if t.find("page") != -1:
+                print()
+                print(t)
+            else:
+                print(t)
+
+        driver.quit()
 
 
 
-
-finally:
-    time.sleep(3)
-    driver.quit()
+if __name__ == "__main__":
+    print("가수명을 입력하세요: ")
+    Artist = input()
+    Crawler(Artist)
